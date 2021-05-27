0dc397f363d32c0ce19a555e8f0e4e6791143519
1187036429@qq.com <1187036429@qq.com>
2021-05-27 14:37:19 +0800

    let 与 var 申明变量是作用域区别!!!!!

--- let.js
+++ let.js
@@ -1,10 +1,28 @@
-{
-    let a = 1
-    var b = 2;
-}
-
-console.log(a)
-console.log(b)
+// {
+//     let a = 1
+//     var b = 2;
+// }
+//
+// console.log(a)
+// console.log(b)
 
 // ReferenceError: a is not defined
 //2
+
+// var a = [];
+// for(var i =0; i < 10; i++){
+//     a[i] = function(){
+//         console.log(i)
+//     }
+// }
+// a[6]();
+
+// for (let i = 0;i<3;i++){
+//     let i = 'abc';
+//     console.log(i)
+// }
+
+//变量提升
+console.log(a)
+var a = 1
+
